acb29979dd1bf7f5c1528fb3859d3d685d3de4e6
Vikask2612 <133359828+Vikask2612@users.noreply.github.com>
2024-09-19 15:36:04 +0000

    comparison_operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);  //basic arithmetc operations
+
+// ########################################################### //
+
+console.log("2" > 1);  //js will automatically convert "2" into number datatype
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);    // these 8 comparisions are not recommended...can make confusions
+
+//  #########################  Strict check ###################################  //
+
+console.log("2" === 2);   //this === will strictly checks the datatypes...so both are different datatypes ..so output will be false
+
